b67e9025588469bddb056cfe7e4f21a7c3b889bd
apsaavedra <apsaavedramira@gmail.com>
2025-03-23 10:21:26 -0300

    API integration

--- app.js
+++ app.js
@@ -1,10 +1,25 @@
+function refreshWeather(response) {
+  let temperature = document.querySelector("#temperature");
+  temperature.innerHTML = Math.round(response.data.temperature.current);
+
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = response.data.city;
+}
+
+function searchCity(city) {
+  let apiKey = "39d5a6021fofb45e9e8c92202c37tc3f";
+
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+
+  axios.get(apiUrl).then(refreshWeather);
+}
+
 function changeCity(event) {
   event.preventDefault();
 
-  let searchCity = document.querySelector("#city-search");
-  let cityElement = document.querySelector("#city");
+  let searchInput = document.querySelector("#city-search");
 
-  cityElement.innerHTML = searchCity.value;
+  searchCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
