b3b991c9ac143309b5ced6febd35fedab309f045
apsaavedra <apsaavedramira@gmail.com>
2025-03-22 23:49:56 -0300

    H1 changing according to submit

--- app.js
+++ app.js
@@ -1,2 +1,11 @@
+function changeCity(event) {
+  event.preventDefault();
 
+  let searchCity = document.querySelector("#city-search");
+  let cityElement = document.querySelector("#city");
 
+  cityElement.innerHTML = searchCity.value;
+}
+
+let searchFormElement = document.querySelector("#search-form");
+searchFormElement.addEventListener("submit", changeCity);
